79a18e4673b20ef7041679eeee9dbf2cc23db8bb
Ridwan ADIO <adioridwan784@gmail.com>
2024-01-09 19:44:34 +0100

    Laravel with Auth

--- zuuro/app/Repositories/AirtimeRepository.php
+++ zuuro/app/Repositories/AirtimeRepository.php
@@ -42,12 +42,19 @@ class AirtimeRepository implements AirtimeRepositoryInterface
 
     public function createVTPassAirtime(array $AirtimeDetails)
     {
-        $response = Http::withHeaders([
-            'api-key'       => "cbec002637753d221bd5dd66dabd3627",
-            'secret-key'    => "SK_41746331032d0670122b4d1ddedd6277c93e8d40e06",
-            'Content-Type'  => 'application/json',
-            'Accept'        => 'application/json'
-        ])->post('https://api-service.vtpass.com/api/pay', $AirtimeDetails);
+        $response = Http::withBasicAuth(
+            'ayotunde@zuuroo.com',
+            'Oyenike1.'
+            )->post(
+                'https://api-service.vtpass.com/api/pay',
+                $AirtimeDetails
+            );
+        // $response = Http::withHeaders([
+        //     'api-key'       => "cbec002637753d221bd5dd66dabd3627",
+        //     'secret-key'    => "SK_41746331032d0670122b4d1ddedd6277c93e8d40e06",
+        //     'Content-Type'  => 'application/json',
+        //     'Accept'        => 'application/json'
+        // ])->post('https://api-service.vtpass.com/api/pay', $AirtimeDetails);
         return $response;
         //https://sandbox.vtpass.com/api/pay
     }
