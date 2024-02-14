9d779bbade73f9acd65d953eb878a14ba7bae220
Ridwan ADIO <adioridwan784@gmail.com>
2024-02-14 09:34:20 +0100

    add layout to report

--- zuuro/resources/views/app/admin/dashboard.blade.php
+++ zuuro/resources/views/app/admin/dashboard.blade.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-
-    <!--favicon-->
-    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png" />
-
-    <title>Zuuro ::| - We provide you best and easy services that make life easier.</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/templatemo-eduwell-style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
-
-</head>
-
-<!-- resources/views/dashboard/dashboard.blade.php -->
-
-@extends('layouts.app') <!-- Assuming you have a master layout, adjust accordingly -->
+@extends('app.admin.admin_layout')
 
 @section('content')
     <div class="container mt-5">
@@ -63,7 +34,7 @@
                 </div>
             </div>
         <div>
-            
+
         <div class="row mt-4">
             <div class="col-md-6">
                 <div class="card">
@@ -74,7 +45,7 @@
                     </div>
                 </div>
             </div>
-        
+
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-body">
@@ -189,8 +160,3 @@
     <script src="{{ asset('js/slick-slider.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
- 
-</body>
-
-</html>
